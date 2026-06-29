8fe7fae9435929f567b7fd1629a2961d7532837f
Denis <sidorenkodenisq@gmail.com>
2026-06-29 16:05:54 +0300

    Added base configuration and dependencies

---
+++ src/main/java/com/task/hotel_service_test_task/HotelServiceTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.task.hotel_service_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/hotel_service_test_task/HotelServiceTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.hotel_service_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
